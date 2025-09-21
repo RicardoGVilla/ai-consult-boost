1b1e97d430ab8ae2549ca4821309593348369ba0
Your Name <your-email@example.com>
2025-09-21 07:08:24 -0400

    light mode default

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
+    <ThemeProvider defaultTheme="light">
       <App />
     </ThemeProvider>
   </StrictMode>,
